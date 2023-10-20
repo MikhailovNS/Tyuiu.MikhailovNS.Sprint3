36f8aa8e782cb54dfdb8e9fbfe8f1da09f4f3833
MikhailovNS <n.s.mikhailov@gmail.com>
2023-10-20 23:04:12 +0500

    Task3

---
+++ Tyuiu.MikhailovNS.Sprint3.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MikhailovNS.Sprint3.Task3.V16.Lib;
+
+namespace Tyuiu.MikhailovNS.Sprint3.Task3.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            DataService ds = new DataService();
+            string value = "ice nice ice creamcc";
+            char chr = 'c';
+            int item = ds.GetCharCount(value, chr);
+            int wait = 6;
+            Assert.AreEqual(wait, item);
+        }
+    }
+}
---
+++ Tyuiu.MikhailovNS.Sprint3.Task3.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MikhailovNS.Sprint3.Task3.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.MikhailovNS.Sprint3.Task3.V16.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("09e7ffb6-42d6-4b28-9f4f-921bf8fa2937")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
