6370f20d4f62924ae9a55b8f815eae77e855edec
MikhailovNS <n.s.mikhailov@gmail.com>
2023-10-25 11:29:30 +0500

    Task5

---
+++ Tyuiu.MikhailovNS.Sprint3.Task5.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MikhailovNS.Sprint3.Task5.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.MikhailovNS.Sprint3.Task5.V20.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f05bb06-9454-4b32-ba2d-be09b9edfe5e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MikhailovNS.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MikhailovNS.Sprint3.Task5.V20.Lib;
+
+namespace Tyuiu.MikhailovNS.Sprint3.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                    *");
+            Console.WriteLine("* Задание #5                                                               *");
+            Console.WriteLine("* Вариант #20                                                              *");
+            Console.WriteLine("* Выполнил: Михайлов Никита Станиславович | АСОиУБ-23-1                    *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*       3   6     x                                                        *");
+            Console.WriteLine("*  y =  E   E  (------)^k, при x = 5                                       *");
+            Console.WriteLine("*      i=1 k=1  sin(x)                                                     *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 6;
+
+            Console.WriteLine("Старт шага первой суммы ряда: "+ startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда: "+ stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда: "+ startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда: "+ stopValue2);
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Сумма ряда равна " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadLine();
+        }
+    }
+}
